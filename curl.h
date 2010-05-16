813adf584d3a621b09740996df8163f5c170524d
Dave Reisner <d@falconindy.com>
2010-05-16 16:12:36 -0400

    curl: add preprocessor constants to help with cookie validation

--- curl.h
+++ curl.h
@@ -27,8 +27,11 @@
 #define AUR_USER_MAX            16
 #define AUR_PASSWORD_MAX        128
 
+#define AUR_URL_NO_PROTO        "aur.archlinux.org"
 #define AUR_LOGIN_URL           "http://aur.archlinux.org/"
 #define AUR_SUBMIT_URL          "http://aur.archlinux.org/pkgsubmit.php"
+
+#define AUR_COOKIE_NAME         "AURSID"
 
 #define AUR_LOGIN_FIELD         "user"
 #define AUR_PASSWD_FIELD        "passwd"
