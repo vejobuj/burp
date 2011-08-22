67be0d961b1e0f36c38a4cca6d662017c2a31402
Dave Reisner <dreisner@archlinux.org>
2011-08-22 08:49:42 -0400

    remove superfluous "if" in FREE

--- util.h
+++ util.h
@@ -32,7 +32,7 @@
 
 #define ALLOC_FAIL(s) do { fprintf(stderr, "could not allocate %zd bytes\n", s); } while(0)
 #define MALLOC(p, s, action) do { p = calloc(1, s); if(!p) { ALLOC_FAIL(s); action; } } while(0)
-#define FREE(x) do { if(x) free((void*)x); x = NULL; } while(0)
+#define FREE(x) do { free((void*)x); x = NULL; } while(0)
 #define STREQ(x,y) strcmp(x,y) == 0
 
 void debug(const char*, ...);
